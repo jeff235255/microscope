91b4875467737d2f9f3b87f295fd71b3affcf0cc
Jeff <jeff235255@gmail.com>
2016-09-05 13:41:14 +0700

    Add template, css for on client folder

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/',
+    domain: ''
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com',
+    domain: ''
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com',
+    domain: ''
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
